6a9eb736ff5dd8b039416a567c5efad31df894cc
ThistyWallrus <thethirstywallrus@gmail.com>
2025-11-25 16:58:09 -0700

    Add files via upload

--- MgmtColor.swift
+++ MgmtColor.swift
@@ -10,16 +10,21 @@ import SwiftUI
 extension Color {
     static let mgmtSilverBlue = Color(red: 162/255, green: 209/255, blue: 241/255) // adjust as needed
 
+    static let mgmtGold = Color(red: 212/255, green: 175/255, blue: 55/255)
     static func mgmtPercentColor(_ percent: Double) -> Color {
         switch percent {
         case 95...100:
+            return .mgmtGold
+        case 90..<95:
             return .mgmtSilverBlue
-        case 85..<95:
+        case 85..<90:
             return .green
         case 75..<85:
             return .yellow
-        default:
+        case 0..<75:
             return .red
+        default:
+            return .gray
         }
     }
 }
